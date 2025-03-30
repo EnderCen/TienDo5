2970551ecf2393d5c212d91a0beee2102462adab
EnderCen <duongchen211@gmail.com>
2025-03-30 21:00:54 +0700

    b

--- SwapFaceAi_copy/public/js/login.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('btn-registers').addEventListener('click', function() {
-    window.location.href = 'http://localhost:8000/register';
-});
--- SwapFaceAi_copy/public/js/main.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("google-login-btn").addEventListener("click", function () {
-    window.location.href = "http://localhost:8000/login"; 
-});
-
